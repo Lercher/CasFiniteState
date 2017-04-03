ecaedc3592cf71b26ba37d988792896259e1a4c4
Martin Lercher <martin.lercher@gmx.de>
2017-04-03 16:53:39 +0200

    Better naming and a better example

--- CasFiniteStateSample/CasConsequenceStartTask.cs
+++ CasFiniteStateSample/CasConsequenceStartTask.cs
@@ -3,14 +3,15 @@ using System.Activities;
 
 namespace CasFiniteStateSample
 {
-    public class CasStartTask : CodeActivity
+    public class CasConsequenceStartTask : CodeActivity
     {
         public InArgument<string> TaskId { get; set; }
+        public int RuleID { get; set; }
 
         protected override void Execute(CodeActivityContext context)
         {
             string tid = context.GetValue(TaskId);
-            Console.WriteLine("Activate Task {0}", tid);
+            Console.WriteLine("Activate Task {0} if Rule {1} is true", tid, RuleID);
         }
     }
 
